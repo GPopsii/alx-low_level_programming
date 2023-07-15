b5174b9a54ce895a39dfaeae954d6f127304f89b
GPopsii <oluwapopsiigee@gmail.com>
2023-07-15 07:24:44 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - My Entery point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++
+		}
+	putchar('\n');
+	return (0);
