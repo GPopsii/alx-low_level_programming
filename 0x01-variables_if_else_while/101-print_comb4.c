889c2d4ab254a37d2b601b26009f930a7e0b7e04
GPopsii <oluwapopsiigee@gmail.com>
2023-07-16 07:02:49 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- *  main - Print all possible different combinations of these digits
- *  Return: Always 0 (Success)
+ * main - Print all possible different combinations of these digits
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int ones = '0';
 	int tens = '0';
-	int hundred = '0';
+	int hundreds = '0';
 
-	for (hundreds = '0'; hundred <= '9'; hundreds++)
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
 		for (tens = '0'; tens <= '9'; tens++)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+				if (!((ones == tens) || (tens == hundreds) || (tens == ones) || (hundreds > tens)))
 				{
 					putchar(hundreds);
 					putchar(tens);
